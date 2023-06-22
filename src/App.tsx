a57876fde8b27618858f04cad716344f60040c08
Radomir Petkov <105939480+RadomirPetkov@users.noreply.github.com>
2023-06-22 15:26:49 +0300

    Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Navbar } from './components/Navbar'
+import { Home } from './components/Home'
 
 function App () {
   return (
-    <h1>Hello</h1>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<h1>About</h1>} />
+      </Routes>
+    </>
   )
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import arrowDown from '../pictures/arrowDown.png'
+import arrowUp from '../pictures/arrowup.jpeg'
+import image from '../pictures/image.jpeg'
+
+export const Navbar = () => {
+  const linkClassName = 'hover:bg-blue-300 p-3 rounded-full'
+  const [navigation, setNavigation] = useState(false)
+
+  return (
+    <div className="bg-black flex-wrap">
+      <div>
+        <Link to={'/'}>
+        <img src={image} alt="" className='h-16 absolute top-10 left-10 rounded-full hidden sm:block sm:absolute md:h-24 xl:h-32 xl:top-5'/>
+        </Link>
+      </div>
+      <h2 className="pt-5 text-2xl font-bold">SilverGlow British Shorthair</h2>
+      <button className='' onClick={() => setNavigation(!navigation)}>
+        {!navigation &&
+          <img src={arrowDown} alt="" className='bg-white text-white rounded-full h-10 mt-3 transition ease-in delay-100 sm:hidden' />
+        }
+        {navigation &&
+          <img src={arrowUp} alt="" className='bg-white text-white rounded-full h-10 mt-3 transition ease-in delay-100 sm:hidden' />
+        }
+      </button>
+      {navigation &&
+        <div className='flex-wrap space-x-1 px-10 py-5 space-y-5 sm:hidden'>
+          <div>
+            <Link to={'/'} className={linkClassName}>Home</Link>
+          </div>
+          <div>
+            <Link to={'/'} className={linkClassName}>Gallery</Link>
+          </div>
+          <div>
+            <Link to={'/'} className={linkClassName}>Available kittens</Link>
+          </div>
+          <div>
+            <Link to={'/'} className={linkClassName}>About us</Link>
+          </div>
+          <div>
+            <Link to={'/'} className={linkClassName}>Contacts</Link>
+          </div>
+        </div>
+      }
+      <div>
+      <div className='flex-wrap space-x-1 px-10 py-5 space-y-5 hidden sm:block sm:p-10 sm:space-x-5'>
+            <Link to={'/'} className={linkClassName}>Home</Link>
+            <Link to={'/'} className={linkClassName}>Gallery</Link>
+            <Link to={'/'} className={linkClassName}>Available kittens</Link>
+            <Link to={'/'} className={linkClassName}>About us</Link>
+            <Link to={'/'} className={linkClassName}>Contacts</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
