ac555d0e30b4068d578ac8365a5c4f77127f0581
Radomir Petkov <105939480+RadomirPetkov@users.noreply.github.com>
2023-07-24 11:21:03 +0300

    remove map from Feedback

--- src/components/Feedback/Feedback.tsx
+++ src/components/Feedback/Feedback.tsx
@@ -3,17 +3,10 @@ import { Comment } from './Comment'
 import { collection, getDocs } from 'firebase/firestore'
 import { db } from '../../firebase-config'
 import { useEffect, useState } from 'react'
-import img from '../../pictures/white.jpg'
-import yellow from '../../pictures/yellow.jpg'
-import ReactModal from 'react-modal'
-import { Modal } from '../features/Modal'
 
 export const Feedback = () => {
-    ReactModal.setAppElement('#root')
     const [snaphots, setSnapshots] = useState([])
     const feedbackRef = collection(db, 'Feedback')
-    const [isOpen, setIsOpen] = useState(false)
-    const [modalImage, setModalImage] = useState('')
     useEffect(() => {
         const getData = async () => {
             const data: any = await getDocs(feedbackRef)
@@ -21,16 +14,10 @@ export const Feedback = () => {
         }
         getData()
     }, [])
-    const handleImgClick = (e) => {
-        setModalImage(e.target.src)
-        setIsOpen(true)
-    }
+
     return (
         <div className='overflow-y-hidden'>
-            <h2 className='m-3 text-xl'>SilverGlows Around The World</h2>
             <div className='w-screen'>
-            <img src={img} alt="" className='w-11/12 m-auto rounded-xl' onClick={handleImgClick}/>
-            <img src={yellow} alt="" className='w-11/12 m-auto rounded-xl my-5' onClick={handleImgClick}/>
             </div>
             <button className='bg-gray-100 text-black p-3 mt-10 rounded-xl'>
                 <Link to="/feedback/comment">
@@ -43,14 +30,6 @@ export const Feedback = () => {
                     return <Comment commentData={snapshot} key={snapshot} />
                 })}
             </div>
-            <ReactModal
-                isOpen={isOpen}
-                contentLabel="Example Modal"
-                contentElement={() => <Modal pic={modalImage} setOpen={setIsOpen} width='w-auto'></Modal>}
-                shouldCloseOnEsc={true}
-                closeTimeoutMS={500}
-            >
-            </ReactModal>
         </div>
     )
 }
