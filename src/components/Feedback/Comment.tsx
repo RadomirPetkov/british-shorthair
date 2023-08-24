eaa6147dc3f1ea1da50dd8730f8c08dfd73006c1
Radomir Petkov <105939480+RadomirPetkov@users.noreply.github.com>
2023-08-24 15:57:24 +0300

    add paws as rating for feedback

--- src/components/Feedback/Comment.tsx
+++ src/components/Feedback/Comment.tsx
@@ -1,13 +1,25 @@
 import { useTranslation } from 'react-i18next'
 import { FaPaw } from 'react-icons/fa6'
+import { IoPawOutline, IoPawSharp } from 'react-icons/io5'
 import { PiPawPrint } from 'react-icons/pi'
+import ReactStars from 'react-rating-stars-component'
 
 export const Comment = (props: any) => {
     const { commentData } = props
-    const { message, ownerName, petName } = commentData
+    const { message, ownerName, petName, rating } = commentData
     const { t } = useTranslation()
     return (
-        <div className='relative overflow-hidden mx-3 my-10 shadow-lg shadow-black border-white border-y-2 rounded-3xl bg-silver max-w-3xl sm:m-auto sm:my-10'>
+        <div className='relative mx-3 my-10 shadow-lg shadow-black border-white border-y-2 rounded-3xl bg-silver max-w-3xl sm:m-auto sm:my-10'>
+            <div className='flex flex-col m-auto items-center gap-3'>
+                <h1 className="m-auto border-blue-300 h4-3 w-fit items-center text-xl">Overall raiting: </h1>
+                <ReactStars
+                    value={rating}
+                    size={30}
+                    edit={false}
+                    emptyIcon={IoPawOutline}
+                    filledIcon={IoPawSharp}
+                />
+            </div>
             <p className='m-auto border-b-2 border-blue-300 p-3 w-fit text-xl'>{t('owner-name')}: {ownerName} </p>
             <p className='m-auto border-b-2 border-blue-300 p-3 w-fit text-xl'>{t('pet-name')}: {petName}</p>
             <p className='p-5'>{message}</p>
--- src/components/Feedback/LeaveAComment.tsx
+++ src/components/Feedback/LeaveAComment.tsx
@@ -4,74 +4,90 @@ import { db } from '../../firebase-config'
 import { v4 as uuidv4 } from 'uuid'
 import { useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
+import ReactStars from 'react-rating-stars-component'
+import { IoPawOutline, IoPawSharp } from 'react-icons/io5'
 
 export const LeaveAComment = () => {
     const { t } = useTranslation()
     const [ownerName, setOwnerName] = useState('')
     const [petName, setPetName] = useState('')
     const [message, setMessage] = useState('')
+    const [stars, setStars] = useState(5)
     const navigate = useNavigate()
     const handleClick = async (e) => {
         e.preventDefault()
         await setDoc(doc(db, 'Feedback', uuidv4()), {
             ownerName,
             petName,
-            message
+            message,
+            rating: stars
         })
         navigate('/feedback')
-        console.log(ownerName, petName, message)
     }
 
-    return <section className="bg-gray-300 rounded-xl h-screen w-screen text-black">
+    return <section className="rounded-xl h-screen w-screen text-black">
         <div className="m-auto px-4 py-16 sm:px-6 lg:px-8 md:w-1/3">
-                <div className="rounded-lg bg-slate-400 p-8 shadow-lg lg:col-span-3 lg:p-12">
-                    <form action="" className="space-y-4">
-                        <div>
-                            <label className="sr-only" htmlFor="name">{t('owner-name')}</label>
-                            <input
-                                className="w-full rounded-lg border-gray-200 p-3 text-sm"
-                                placeholder="Owner name"
-                                type="text"
-                                id="name"
-                                value={ownerName}
-                                onChange={(e) => setOwnerName(e.target.value)}
+            <div className="rounded-lg bg-slate-400 p-8 shadow-lg lg:col-span-3 lg:p-12">
+                <form action="" className="space-y-4">
+                    <div>
+                        <h5 className='pb-0 mb-0 text-xl'>Overall rating:</h5>
+                        <div className='m-auto py-3 flex flex-col items-center'>
+                            <ReactStars
+                                count={5}
+                                value={stars}
+                                size={24}
+                                onChange={(newValue) => setStars(newValue)}
+                                emptyIcon={IoPawOutline}
+                                filledIcon={IoPawSharp}
                             />
                         </div>
+                    </div>
+                    <div>
+                        <label className="sr-only" htmlFor="name">{t('owner-name')}</label>
+                        <input
+                            className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                            placeholder="Owner name"
+                            type="text"
+                            id="name"
+                            value={ownerName}
+                            onChange={(e) => setOwnerName(e.target.value)}
+                        />
+                    </div>
+                    <div>
                         <div>
-                            <div>
-                                <label className="sr-only" htmlFor="cat-name">{t('pet-name')}</label>
-                                <input
-                                    className="w-full rounded-lg border-gray-200 p-3 text-sm"
-                                    placeholder="Pet name"
-                                    type="text"
-                                    id="cat-name"
-                                    value={petName}
-                                    onChange={(e) => setPetName(e.target.value)}
-                                />
-                            </div>
+                            <label className="sr-only" htmlFor="cat-name">{t('pet-name')}</label>
+                            <input
+                                className="w-full rounded-lg border-gray-200 p-3 text-sm"
+                                placeholder="Pet name"
+                                type="text"
+                                id="cat-name"
+                                value={petName}
+                                onChange={(e) => setPetName(e.target.value)}
+                            />
                         </div>
+                    </div>
 
-                        <div>
-                            <label className="sr-only" htmlFor="message">{t('message')}</label>
-                            <textarea
-                                className="w-full rounded-lg border-gray-200 p-5 h-48 text-sm"
-                                placeholder="Message"
-                                id="message"
-                                value={message}
-                                onChange={(e) => setMessage(e.target.value)}
-                            ></textarea>
-                        </div>
+                    <div>
+                        <label className="sr-only" htmlFor="message">{t('message')}</label>
+                        <textarea
+                            className="w-full rounded-lg border-gray-200 p-5 h-48 text-sm"
+                            placeholder="Message"
+                            id="message"
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                        ></textarea>
+                    </div>
 
-                        <div className="mt-4">
-                            <button
-                                className="inline-block w-full rounded-lg bg-black px-5 py-3 font-medium text-white sm:w-auto"
-                                onClick={handleClick}
-                            >
-                                {t('send-feedback')}
-                            </button>
-                        </div>
-                    </form>
-                </div>
+                    <div className="mt-4">
+                        <button
+                            className="inline-block w-full rounded-lg bg-black px-5 py-3 font-medium text-white sm:w-auto"
+                            onClick={handleClick}
+                        >
+                            {t('send-feedback')}
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     </section>
 }
