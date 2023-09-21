5257ac4ec169265e9dbba9b0daa772a0549ef437
Radomir Petkov <105939480+RadomirPetkov@users.noreply.github.com>
2023-09-21 09:38:00 +0300

    add google search description

--- src/components/features/Messanger.tsx
+++ src/components/features/Messanger.tsx
@@ -3,8 +3,8 @@ import MessengerCustomerChat from 'react-messenger-customer-chat'
 export const Messanger = () => {
     return (
         <MessengerCustomerChat
-            pageId="361392053995493"
-            appId="652662329654460"
+            pageId='1520539621422715'
+            appId='652662329654460'
         />
     )
 }
